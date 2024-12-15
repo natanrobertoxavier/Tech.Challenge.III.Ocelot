bc9263c21bd2575ab10af246a91dbf64e8204598
Natan Roberto Xavier <121309547+natanrobertoxavier@users.noreply.github.com>
2024-12-15 17:52:24 -0300

    feat: added basic config

---
+++ Tech.Challenge.Api.Gateway/Tech.Challenge.Api.Gateway/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.IdentityModel.Tokens;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:7183/";
+        options.TokenValidationParameters = new TokenValidationParameters()
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddOcelot();
+
+var app = builder.Build();
+
+app.UseOcelot();
+
+app.Run();
